4f3b2ef9b0c9e9998b20ec66c595a27069738b32
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-04-26 20:06:06 -0500

    feat: Add Searchbar component
    This is the search bar component in the navbar. Some of the logic and
    styling for this component is in the Navbar files.

---
+++ src/Searchbar.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+
+const SearchBar = ({ onSearch }) => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSearch(searchTerm);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Search..."
+        value={searchTerm}
+        onChange={handleChange}
+      />
+      <button type="submit">Search</button>
+    </form>
+  );
+};
+
+export default SearchBar;
